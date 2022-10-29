31d17fa25e6d5b85b99e33fd8059d1ecd64f6a37
ksemilla <kevinsemilla@gmail.com>
2022-10-30 01:14:12 +0800

    React and vite setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="">
+      test
+      <div className='text-3xl'>test</div>
+    </div>
+  )
+}
+
+export default App
